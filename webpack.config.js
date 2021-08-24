f24e5e708a1403e21fa05f5ebf9994fca7d39d92
Pavel Gushchin <pavel_gushchin@mail.ru>
2021-08-24 21:53:53 +0700

    Finished project configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,106 +1,95 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const {CleanWebpackPlugin} = require('clean-webpack-plugin')
-const CopyPlugin = require('copy-webpack-plugin')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
 
 const isProduction = process.env.NODE_ENV == 'production';
 const isDevelopment = !isProduction;
 
 
-const outputFilename = (ext) => isProduction ? `bundle.[hash].${ext}` : `bundle.${ext}`
-
-const jsLoaders = () => {
-    const loaders = ['babel-loader'];
-
-    if (isDevelopment) {
-        loaders.push('eslint-loader')
-    }
-
-    return loaders;
-}
-
-
 const config = {
-    context: path.resolve(__dirname, 'src'),
-    entry: './index.js',
-    output: {
-        filename: outputFilename('js'),
-        path: path.resolve(__dirname, 'dist'),
+  context: path.resolve(__dirname, 'src'),
+  mode: 'development',
+  entry: './index.js',
+  output: {
+    filename: isProduction ? 'bundle.[fullhash].js' : 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    open: true,
+    port: 9000,
+    hot: isDevelopment,
+  },
+  devtool: isDevelopment ? 'source-map' : false,
+  resolve: {
+    extensions: ['js'],
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '@core': path.resolve(__dirname, 'src/core'),
     },
-    devServer: {
-        open: true,
-        host: 'localhost',
-        port: 3000,
-        hot: isDevelopment,
-    },
-    devtool: isDevelopment ? 'source-map' : false,
-    resolve: {
-        extensions: ['js'],
-        alias: {
-            '@': path.resolve(__dirname, 'src'),
-            '@core': path.resolve(__dirname, 'src/core'),
+  },
+
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+      minify: {
+        removeComments: isProduction,
+        collapseWhitespace: isProduction,
+      },
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'src/favicon.ico'),
+          to: path.resolve(__dirname, 'dist'),
         },
-    },
+      ],
+    }),
+    new MiniCssExtractPlugin({
+      filename: isProduction ? 'bundle.[fullhash].css' : 'bundle.css',
+    }),
+  ],
 
-    plugins: [
-        new CleanWebpackPlugin(),
-        new HtmlWebpackPlugin({
-            template: 'index.html',
-            minify: {
-                removeComments: isProduction,
-                collapseWhitespace: isProduction,
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/i,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              hmr: isDevelopment,
+              reloadAll: true,
             },
-        }),
-        new CopyPlugin({
-            patterns: [
-                {
-                    from: path.resolve(__dirname, 'src/favicon.ico'),
-                    to: path.resolve(__dirname, 'dist'),
-                },
-            ],
-        }),
-        new MiniCssExtractPlugin({
-            filename: outputFilename('css'),
-        }),
-    ],
-
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/i,
-                exclude: /node_modules/,
-                use: jsLoaders(),
-            },
-            {
-                test: /\.s[ac]ss$/i,
-                use: [
-                    {
-                        loader: MiniCssExtractPlugin.loader,
-                        options: {
-                            hmr: isDevelopment,
-                            reloadAll: true,
-                        },
-                    },
-                    'css-loader',
-                    'sass-loader'
-                ],
-            },
-            {
-                test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-                type: 'asset',
-            },
+          },
+          'css-loader',
+          'sass-loader',
         ],
-    },
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+        type: 'asset',
+      },
+    ],
+  },
 };
 
 
 module.exports = () => {
-    if (isProduction) {
-        config.mode = 'production';
-    } else {
-        config.mode = 'development';
-    }
+  if (isProduction) {
+    config.mode = 'production';
+  } else {
+    config.mode = 'development';
+    config.plugins.push(new ESLintPlugin());
+  }
 
-    return config;
+  return config;
 };
